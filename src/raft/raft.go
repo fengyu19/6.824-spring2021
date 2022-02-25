635313bd035bea1935bee71bcb3fe1e7cafb574d
fengyu <fengyuyan@citydnatech.com>
2022-02-25 17:53:15 +0800

    add: Raft 2A finished

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -19,9 +19,12 @@ package raft
 //
 
 import (
+	"math/rand"
 	//	"bytes"
 	"sync"
 	"sync/atomic"
+	"time"
+
 	//	"6.824/labgob"
 	"6.824/labrpc"
 )
@@ -37,8 +40,8 @@ const (
 const NULL int = -1
 
 type Log struct {
-	Term  	int
-	Command interface{}
+	Term  	int				//从领导人处收到的任期号
+	Command interface{}		//状态机需要执行的命令
 }
 
 //
@@ -80,17 +83,17 @@ type Raft struct {
 	state State
 
 	// Persistent state on all servers:
-	currentTerm int
-	votedFor 	int
-	log 		[]Log
+	currentTerm int			//服务器最后知道的任期号，从0开始递增
+	votedFor 	int			//在当前任期内收到选票的候选人，没有的话为null
+	log 		[]Log		//日志条目
 
 	// Volatile state on all servers:
-	commitIndex int
-	lastApplied int
+	commitIndex int			//已知的被提交的最大日志条目的索引值，0开始递增
+	lastApplied int			//被状态机执行的最大日志条目的索引值，0开始递增
 
 	// Volatile state on leaders:
-	nextIndex	[]int
-	matchIndex	[]int
+	nextIndex	[]int		//对于每一台服务器，记录需要发给它的下一个日志条目索引
+	matchIndex	[]int		//对于每一台服务器，记录已经复制到该服务器的日志的最高索引值，从0开始递增
 
 	// channel
 	applyCh 	chan ApplyMsg
@@ -107,7 +110,7 @@ func (rf *Raft) GetState() (int, bool) {
 	var isleader bool
 	// Your code here (2A).
 	term = rf.currentTerm
-	isleader = rf.state == Leader
+	isleader = (rf.state == Leader)
 	return term, isleader
 }
 
@@ -178,10 +181,10 @@ func (rf *Raft) Snapshot(index int, snapshot []byte) {
 //
 type RequestVoteArgs struct {
 	// Your data here (2A, 2B).
-	Term			int
-	CandidateId 	int
-	LastLogIndex	int
-	LastLogTerm		int
+	Term			int 		//候选人的任期号
+	CandidateId 	int			//请求投票的候选人id
+	LastLogIndex	int			//候选人最新日志条目的索引值
+	LastLogTerm		int			//候选人最新日志条目对应的任期号
 }
 
 //
@@ -190,8 +193,8 @@ type RequestVoteArgs struct {
 //
 type RequestVoteReply struct {
 	// Your data here (2A).
-	Term			int
-	VotedGranted	bool
+	Term			int			//目前的任期号，用于候选人更新自己
+	VotedGranted	bool		//如果候选人收到选票，则为true
 }
 
 //
@@ -199,6 +202,29 @@ type RequestVoteReply struct {
 //
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	// Your code here (2A, 2B).
+	if args.Term > rf.currentTerm { // leader, candidate will be follower
+		rf.beFollower(args.Term)
+		send(rf.voteCh)
+	}
+	success := false
+	if args.Term < rf.currentTerm {
+
+	}else if rf.votedFor != NULL && rf.votedFor != args.CandidateId {
+		
+	}else if args.LastLogTerm < rf.getLastLogTerm() {
+		// candidate log is not new
+	}else if args.LastLogTerm == rf.getLastLogTerm() && args.LastLogIndex < rf.getLastLogIdx() {
+		// candidate log is not new
+	}else {
+		//vote for him
+		rf.votedFor = args.CandidateId
+		success = true
+		rf.state = Follower
+		send(rf.voteCh)
+	}
+	
+	reply.Term = rf.currentTerm
+	reply.VotedGranted = success
 }
 
 //
@@ -237,17 +263,17 @@ func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *Reques
 
 
 type AppendEntriesArgs struct {
-	Term			int
-	LeaderId 		int
-	PreLogIndex		int
-	PrevLogTerm		int
-	Entries 		[]Log
-	LeaderCommit	int
+	Term			int 		//领导人的任期号
+	LeaderId 		int			//领导人的id，为了其他服务器能重定向到客户端
+	PrevLogIndex	int			//最新日志之前的日志的索引值
+	PrevLogTerm		int			//最新日志之前的日志的领导人任期号
+	Entries 		[]Log		//将要存储的日志条目（heartbeats时为空）
+	LeaderCommit	int			//领导人提交的日志条目索引值
 }
 
 type AppendEntriesReply struct {
-	Term 			int
-	Success			bool
+	Term 			int			//当前的任期号，用于领导人更新自己的任期号
+	Success			bool		//如果其他服务器包含能够匹配上 prevLogTerm 和 prevLogIndex 时为真
 }
 
 
@@ -338,12 +364,192 @@ func Make(peers []*labrpc.ClientEnd, me int,
 	rf.nextIndex = make([]int, len(peers))
 	rf.matchIndex = make([]int, len(peers))
 
+	// because goroutine only send the chan to below goroutine, to avoid block, need 1 buffer
+	rf.applyCh = applyCh
+	rf.voteCh = make(chan bool, 1)
+	rf.appendLogCh = make(chan bool, 1)
 	// initialize from state persisted before a crash
 	rf.readPersist(persister.ReadRaftState())
+
+	// because from the hint the test requires that the leader send heartbeats RPCs no more than ten times per seconds
+	heartbeatTime := time.Duration(100) * time.Millisecond
+	
+	//from hint: You'll need to write code that takes actions periodically or after delays in time.
+	//The easiest way to do this is to create a goroutine with a loop that calls time.Sleep()
+	go func() {
+		for  {
+			electionTime := time.Duration(rand.Intn(100) + 300) * time.Millisecond
+			switch rf.state {
+			case Follower, Candidate:
+				select {
+				case <-rf.voteCh:
+				case <-rf.appendLogCh:
+				case <-time.After(electionTime):
+					rf.beCandidate() //switch to candidate, then start election
+				}
+			case Leader:
+				rf.startAppendLog()
+				time.Sleep(heartbeatTime)
+			}
+		}
+	}()
 
 	// start ticker goroutine to start elections
 	go rf.ticker()
 
 
 	return rf
+}
+
+func (rf *Raft) beCandidate()  {
+	rf.state = Candidate
+	rf.currentTerm++
+	rf.votedFor = rf.me //vote myself first
+	// ask for other's vote
+	go rf.startElection()
+}
+
+func (rf *Raft) startElection()  {
+	args := RequestVoteArgs{
+		rf.currentTerm,
+		rf.me,
+		rf.getLastLogIdx(),
+		rf.getLastLogTerm(),
+	}
+	var votes int32 = 1
+	for i := 0; i < len(rf.peers); i++ {
+		if i == rf.me {
+			continue
+		}
+		go func(idx int) {
+			// 1. send req
+			reply := &RequestVoteReply{}
+			ret := rf.sendRequestVote(idx, &args, reply)
+			if ret {
+				if reply.Term > rf.currentTerm {
+					rf.beFollower(reply.Term)
+					send(rf.voteCh)
+					return
+				}
+				if rf.state != Candidate {
+					return
+				}
+				if reply.VotedGranted {
+					atomic.AddInt32(&votes, 1)
+				}
+				if atomic.LoadInt32(&votes) > int32(len(rf.peers) / 2) {
+					rf.beLeader() // update status
+					send(rf.voteCh)
+				}
+			}
+		}(i)
+	}
+}
+
+func send(ch chan bool)  {
+	select {
+	case <-ch: // if already set, consume it then re-sent to avoid block
+	default:
+	}
+	ch <- true
+}
+
+func (rf *Raft) getLastLogIdx() int {
+	return len(rf.log) - 1
+}
+
+func (rf *Raft) getLastLogTerm() int {
+	idx := rf.getLastLogIdx()
+	if idx < 0 {
+		return -1
+	}
+	return rf.log[idx].Term
+}
+
+func (rf *Raft) getPrevLogIdx(i int) int {
+	return rf.nextIndex[i] - 1
+}
+
+func (rf *Raft) getPrevLogTerm(i int) int {
+	prevLogIdx := rf.getPrevLogIdx(i)
+	if prevLogIdx < 0 {
+		return -1
+	}
+	return rf.log[prevLogIdx].Term
+}
+
+func (rf *Raft) beFollower(term int)  {
+	rf.state = Follower
+	rf.votedFor = NULL
+	rf.currentTerm = term
+}
+
+func (rf *Raft) beLeader()  {
+	if rf.state != Candidate {
+		return
+	}
+	rf.state = Leader
+	// initialize leader data
+	rf.nextIndex = make([]int, len(rf.peers))
+	rf.matchIndex = make([]int, len(rf.peers))
+	for i := 0; i < len(rf.nextIndex); i++ {
+		rf.nextIndex[i] = rf.getLastLogIdx() + 1
+	}
+}
+
+func (rf *Raft) sendAppendEntries(server int, args *AppendEntriesArgs, reply *AppendEntriesReply) bool {
+	ok := rf.peers[server].Call("Raft.AppendEntries", args, reply)
+	return ok
+}
+
+// AppendEntries RPC handler
+func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) { // now only for heartbeat
+	if args.Term > rf.currentTerm { // leader, candidate will be follower
+		rf.beFollower(args.Term)
+		send(rf.appendLogCh)
+	}
+	success := false
+	PrevLogIndexTerm := -1
+	if args.PrevLogIndex >= 0 {
+		PrevLogIndexTerm = rf.log[args.PrevLogIndex].Term
+	}
+	if args.Term < rf.currentTerm {
+		//ret false
+	} else if PrevLogIndexTerm != args.PrevLogTerm {
+		//ret false
+	}
+	reply.Term = rf.currentTerm
+	reply.Success = success
+}
+
+// from leader action
+func (rf *Raft) startAppendLog()  {
+	for i := 0; i < len(rf.peers); i++ {
+		if i == rf.me {
+			continue
+		}
+		go func(idx int) {
+			if rf.state != Leader {
+				return
+			}
+			args := AppendEntriesArgs{
+				rf.currentTerm,
+				rf.me,
+				rf.getPrevLogIdx(idx),
+				rf.getPrevLogTerm(idx),
+				make([]Log, 0), // will implement real send log
+				rf.commitIndex,
+			}
+			reply := &AppendEntriesReply{}
+
+			ret := rf.sendAppendEntries(idx, &args, reply)
+			if !ret {
+				return
+			}
+			if reply.Term > rf.currentTerm {
+				rf.beFollower(reply.Term)
+				return
+			}
+		}(i)
+	}
 }
